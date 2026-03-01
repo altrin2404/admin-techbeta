33dd93d6125de2da885df94f6f8e0eca2b0deed2
Altrin Benser <altrin.sxcce.edu.in>
2026-03-01 17:57:22 +0530

    Update registrationService

--- src/lib/registrationService.ts
+++ src/lib/registrationService.ts
@@ -91,6 +91,7 @@ export const addRegistration = async (data: Omit<Registration, "id" | "status" |
             if (webhookUrl) {
                 await fetch(webhookUrl, {
                     method: 'POST',
+                    mode: 'no-cors',
                     headers: { 'Content-Type': 'text/plain;charset=utf-8' },
                     body: JSON.stringify({
                         ...sanitizedData,
